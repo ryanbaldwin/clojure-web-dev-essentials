9eff29c05be9590964d36f35890bca65b4014e4d
ryanbaldwin <ryanbaldwin@gmail.com>
2014-11-17 21:37:15 -0500

    first commit of second draft

---
+++ src/hipstr/routes/home.clj
@@ -0,0 +1,15 @@
+(ns hipstr.routes.home
+  (:require [compojure.core :refer :all]
+            [hipstr.layout :as layout]
+            [hipstr.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
---
+++ src/hipstr/util.clj
@@ -0,0 +1,8 @@
+(ns hipstr.util
+  (:require [noir.io :as io]
+            [markdown.core :as md]))
+
+(defn md->html
+  "reads a markdown file from public/md and returns an HTML string"
+  [filename]
+  (md/md-to-html-string (io/slurp-resource filename)))
