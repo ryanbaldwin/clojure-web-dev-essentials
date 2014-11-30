de60c569fad103024adf2b1d13b076460ea5cc1c
ryanbaldwin <ryanbaldwin@gmail.com>
2014-11-30 10:21:33 -0500

    resources/public/css/screen.css

--- src/hipstr/routes/home.clj
+++ src/hipstr/routes/home.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer :all]
             [hipstr.layout :as layout]
             [hipstr.util :as util]
+            [hipstr.validators.user-validator :as v]
             [ring.util.response :as response]))
 
 (defn home-page []
@@ -21,9 +22,9 @@
   (layout/render "signup.html"))
 
 (defn signup-page-submit [user]
-  #_(let [errors (signup/validate-signup user)]
+  (let [errors (v/validate-signup user)]
     (if (empty? errors)
-      (response/redirect "/signup-success.html")
+      (response/redirect "/signup-success")
       (layout/render "signup.html" (assoc user :errors errors)))))
 
 (defroutes home-routes
--- src/hipstr/validators/user_validator.clj
+++ src/hipstr/validators/user_validator.clj
@@ -1,21 +1,35 @@
 (ns hipstr.validators.user-validator
-  (:require [validateur.validation :refer :all]))
+  (:require [noir.validation :as v]
+            [validateur.validation :refer :all]))
+
+(def email-validator
+  (validation-set
+   (validate-with-predicate :email
+                            #(v/is-email? (:email %))
+                            :message-fn (fn [validation-map]
+                                          (if (v/has-value? (:email validation-map))
+                                            "The email's format is incorrect"
+                                            "is a required field")))))
+
+(def username-validator
+  (validation-set
+   (format-of :username
+              :format #"^[a-zA-Z0-9_]*$"
+              :blank-message "is a required field"
+              :message "Only letters, numbers, and underscores allowed.")))
+
+(def password-validator
+  (validation-set
+   (length-of :password
+              :within (range 8 101)
+              :blank-message "is a required field."
+              :message-fn (fn [type m attribute & args]
+                            (if (= type :blank)
+                              "is a required field"
+                              "Passwords must be between 8 and 100
+                               characters long.")))))
 
 (defn validate-signup [signup]
-  "Validates teh incoming map of values from our signup form,
-   and returns a set of error messages for any invalid key.
-   Expects signup to have :username, :email, and :password."
-  (let [v (validation-set
-           (presence-of #{:email :password}
-                        :message "is a required field.")
-           (format-of :username
-                      :format #"^[a-zA-Z0-9_]*$"
-                      :message "Only letters, numbers, and underscores allowed."
-                      :blank-message "is a required field")
-           (length-of :password
-                      :within (range 8 101)
-                      :message-fn (fn [type m attribute & args]
-                                    (if (= type :blank)
-                                      "is a required field"
-                                      "Password must be between 8 and 100 characters long.")))]
-    (v signup)))
+  "Validates the incoming signup map and returns a set of error
+   messages for any invalid field."
+   ((compose-sets email-validator username-validator password-validator) signup))
